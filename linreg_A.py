b3bb58828d194fc6c76ba7dcc3ceb7b6fa346284
Jonathan <s174182@student.dtu.dk>
2020-11-07 11:25:07 +0100

    Færdig version

--- linreg_A.py
+++ linreg_A.py
@@ -15,19 +15,20 @@ from toolbox_02450 import rlr_validate
 from loadingdata import * 
 meanX = X.mean(axis = 0)
 stdX = X.std(ddof=1,axis=0)
-
 y=X[:,0] # puts the y values equal to ozone
 X = X - np.ones((N,1))*meanX
 X=X/stdX
-X=X[:,1:-1]  #cut out the ozone
+X=np.hstack((X[:,1:-1],y_new))  #cut out the ozone and day
+                                #and combine it with 
 
 
 N, M = X.shape
 # Add offset attribute
 X = np.concatenate((np.ones((X.shape[0],1)),X),1)
 
-attributeNames = [u'Offset']+attributeNames[1:-1].tolist() #converts it to a string,
-                                                        #removes the attribute ozone and season(day)
+#converts it to a string,
+#removes the attribute ozone and season(day)
+attributeNames = [u'Offset']+attributeNames[1:-1].tolist()+['Spring','Summer','Fall','Winter'] 
                                                             
 print(attributeNames)
 M = M+1
@@ -37,7 +38,7 @@ M = M+1
 K = 10
 CV = model_selection.KFold(K, shuffle=True)
 # Values of lambda
-lambdas = np.power(10.,range(-2,3))
+lambdas = np.power(2.,range(-2,8))
 
 Error_train = np.empty((K,1))
 Error_test = np.empty((K,1))
@@ -139,6 +140,7 @@ print('- R^2 test:     {0}\n'.format((Error_test_nofeatures.sum()-Error_test_rlr
 
 print('Weights in last fold:')
 for m in range(M):
+    print(m)
     print('{:>15} {:>15}'.format(attributeNames[m], np.round(w_rlr[m,-1],2)))
 
 
