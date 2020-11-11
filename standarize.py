49304c586baf9843119b222623ec8efea95fc495
Andreas With Aspe <s174197@student.dtu.dk>
2020-11-11 10:41:41 +0100

    Færdig version

--- standarize.py
+++ standarize.py
@@ -6,12 +6,10 @@ Created on Tue Nov 10 10:28:26 2020
 @author: andreasaspe
 """
 
-from loadingdata import *
+import numpy as np
 
-def standarize():
-  stdX = X.std(ddof=1,axis=0)
-  Y = X - np.ones((N,1))*X.mean(axis=0)
-  Y=Y/stdX #normalise the data by devision of the
-                  #standard deviation
-  
-  return Y
+def standarize(X):
+  mu = np.mean(X, 0)
+  sigma = np.std(X, 0)
+  X = (X - mu) / sigma
+  return X
